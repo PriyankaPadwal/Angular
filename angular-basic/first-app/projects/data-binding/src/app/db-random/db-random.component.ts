c2c681b7207bbc793b74e62ffd0bf433773acabb
priyankapadwal <priyapadwal17@gmail.com>
2019-11-27 17:12:42 +0530

    data binding

--- angular-basic/first-app/projects/data-binding/src/app/db-random/db-random.component.ts
+++ angular-basic/first-app/projects/data-binding/src/app/db-random/db-random.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./db-random.component.css']
 })
 export class DbRandomComponent implements OnInit {
-  boxbknd='white'
+  boxbknd=''
+  boxbknd1=''
+  boxbknd2=''
+  boxbknd3=''
+
   constructor() { }
 
   ngOnInit() {
@@ -15,20 +19,26 @@ export class DbRandomComponent implements OnInit {
   onMouseEnter(event: MouseEvent){
     console.log("entered",event)
     this.boxbknd=this.randomColor()
+    this.boxbknd1=this.randomColor()
+    this.boxbknd2=this.randomColor()
+    this.boxbknd3=this.randomColor()
   }
   onMouseLeave(event: MouseEvent){
     console.log("Exist",event)
     this.boxbknd='white'
+    this.boxbknd1='white'
+    this.boxbknd2='white'
+    this.boxbknd3='white'
   }
 
   randomColor(): string{
-  let red=Math.floor(Math.random()*256);   
-  let green=Math.floor(Math.random()*256);   
-  let blue=Math.floor(Math.random()*256);   
+  let red=Math.floor(Math.random()*255);   
+  let green=Math.floor(Math.random()*255);   
+  let blue=Math.floor(Math.random()*255);   
 
   console.log("red-"+red,"green-"+green,"blue-"+blue);
   
-    return `rgb(${red},${green},${blue})`
+  return `rgb(${red},${green},${blue})`
   }
 
 
