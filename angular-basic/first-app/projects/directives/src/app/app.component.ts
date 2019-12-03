c4f5cfa14fddf3e9e4b9f276fa42019666d6d874
priyankapadwal <priyapadwal17@gmail.com>
2019-12-03 19:11:33 +0530

    directives : structural directive (*ngIf ,*ngFor)

--- angular-basic/first-app/projects/directives/src/app/app.component.ts
+++ angular-basic/first-app/projects/directives/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+isPara=false  
   title = 'directives';
+  onClick(evt:any){
+   this.isPara=!this.isPara
+   console.log(evt);
+  }
 }
